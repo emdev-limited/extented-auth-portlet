a763d1d17ebadb2dbfe3eef00b39ed3daf828b70
Alexey Melnikov <alexey.melnikoff@gmail.com>
2013-03-20 19:14:24 +0400

    new package for actions

--- src/main/java/ru/emdev/auth/Constants.java
+++
@@ -1,8 +0,0 @@
-package ru.emdev.auth;
-
-public abstract class Constants {
-
-	public static final String AUTH_CACHE_NAME = "customAuthCache";
-
-	public static final String KEY_SESSIONS_CACHE = "sessionsCache";
-}
